d26555263d7a1e1088347bdb4eaaa766feef6869
ppinheiro363 <henrique363@gmail.com>
2023-02-24 11:06:25 -0300

    Adicionado a função atualiza_planilha.

--- gspread_config/scope.py
+++ gspread_config/scope.py
@@ -1,5 +1,8 @@
 from google.oauth2.service_account import Credentials
-import gspread
+import gspread as gs
+from gspread_dataframe import get_as_dataframe, set_with_dataframe
+
+import pandas as pd
 
 scopes = [
     'https://www.googleapis.com/auth/spreadsheets',
@@ -11,4 +14,13 @@ credentials = Credentials.from_service_account_file(
     scopes = scopes
 )
 
-autoriza_google = gspread.authorize(credentials)
+autoriza_google = gs.authorize(credentials)
+
+
+def atualiza_planilha(planilha: str, worksheet: str, dados: str) -> None:
+    abre_planilha = gs.open(planilha)
+    seleciona_worksheet = abre_planilha.worksheet(worksheet)
+    dados_dataframe = pd.DataFrame(dados)
+    
+    set_with_dataframe(seleciona_worksheet, dados_dataframe)
+    
